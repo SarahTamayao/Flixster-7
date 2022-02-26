a402177231e1a1d6347c6d1f73b2a322990792a2
Blake Hakkila <blake.austin.hakkila@gmail.com>
2022-02-25 22:02:23 -0800

    Added JSON parsing to Movie data class

--- app/src/main/java/com/example/flixster/MainActivity.kt
+++ app/src/main/java/com/example/flixster/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.flixster
 
-import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import org.json.JSONException
 
 private const val TAG = "MainActivity"
 private const val NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=" + BuildConfig.MOVIE_DB_API_KEY
 class MainActivity : AppCompatActivity() {
+    private val movies = mutableListOf<Movie>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,9 +27,14 @@ class MainActivity : AppCompatActivity() {
 
             override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
                 Log.i(TAG, "onSuccess: JSON data $json")
-                val movieJsonArray = json.jsonObject.getJSONArray("results")
+                try {
+                    val movieJsonArray = json.jsonObject.getJSONArray("results")
+                    movies.addAll(Movie.fromJsonArray(movieJsonArray))
+                    Log.i(TAG, "Movie list $movies")
+                } catch (e: JSONException) {
+                    Log.e(TAG, "Encountered exception $e")
+                }
             }
-
         })
     }
 }
